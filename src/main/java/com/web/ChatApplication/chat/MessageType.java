7e60913dd215b06ae3a56991d32954ec72903a68
harivx1 <hari.v4p@gmail.com>
2024-08-25 21:11:42 +0530

    first commit

---
+++ src/main/java/com/web/ChatApplication/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.web.ChatApplication.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVER,
+}
---
+++ src/main/java/com/web/ChatApplication/config/WebSocketEventLister.java
@@ -0,0 +1,20 @@
+package com.web.ChatApplication.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventLister {
+
+        @EventListener
+        public void handleWebSocketDisconnectListener(
+                SessionDisconnectEvent event
+        ){
+            //to -- be impl
+        }
+}
